0f75d284765bdeba75d75cc4d9837a0378c1ed07
Jan Montalvo <55473580+JanMichaelSE@users.noreply.github.com>
2023-11-19 10:39:24 -0400

    Updated neovim theme to use catppuccin

--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -1,14 +1,13 @@
-require('rose-pine').setup({
-    disable_background = true
+require("catppuccin").setup({
+	transparent_background = true,
 })
 
-function ColorMyPencils(color)
-	color = color or "rose-pine"
+function SetBackgroundTransparency(color)
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
-ColorMyPencils()
+SetBackgroundTransparency()
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -69,12 +69,20 @@ require('lazy').setup({
     },
   },
 
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'rose-pine'
+  --   end,
+  -- },
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
