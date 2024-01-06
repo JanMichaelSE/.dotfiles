8184c9750bdc95bc3d4d21ee4dd961488643bb12
Jan Montalvo <55473580+JanMichaelSE@users.noreply.github.com>
2024-01-06 16:20:48 -0400

    Modified harpoon, changed tmux to open projects in same window and golang plugins.

--- config/nvim/after/plugin/harpoon.lua
+++ config/nvim/after/plugin/harpoon.lua
@@ -6,5 +6,5 @@ vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<M-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<M-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<M-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<M-m>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<M-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<M-l>", function() ui.nav_file(4) end)
